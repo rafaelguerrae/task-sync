7a166b4bd52c14ffa2277efe361d8bec68f96703
RafaelGuerraE <gueerae@gmail.com>
2025-03-05 22:18:36 -0300

    refactor(gradle): learning how to use the toml to implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
 
+    implementation (libs.androidx.material.icons.extended)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
